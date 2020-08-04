ce4b3486a01deaa8b40779f06ce3fabf4eb54655
Antonio Calatayud <antonio.calatayud@adas.co.uk>
2020-08-04 10:34:38 +0100

    Add new template

---
+++ H2020.IPMDecisions.EML.Core/Dtos/DataShareDto.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace H2020.IPMDecisions.EML.Core.Dtos
+{
+    public class DataShareDto
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string ToAddress { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string DataRequesterEmail { get; set; }
+    }
+}
