562a202e22da4cfda01f8bee4564bcfb757ffcf5
Antonio Calatayud <antonio.calatayud@adas.co.uk>
2021-06-15 13:53:27 +0200

    Extrac hardcode strings

--- H2020.IPMDecisions.EML.Core/EmailTemplates/EmailTemplates.cs
+++ H2020.IPMDecisions.EML.Core/EmailTemplates/EmailTemplates.cs
@@ -1,6 +1,11 @@
 namespace H2020.IPMDecisions.EML.Core.EmailTemplates
 {
     public class EmailTemplates
-    {        
+    {
+        public const string DataShareEmailTemplatePath = "EmailTemplates.DataShareEmailTemplate";
+        public const string ForgotPasswordEmailTemplatePath = "EmailTemplates.ForgotPasswordEmailTemplate";
+        public const string InactiveUserEmailTemplatePath = "EmailTemplates.InactiveUserEmailTemplate";
+        public const string ReConfirmEmailEmailTemplatePath = "EmailTemplates.ReConfirmEmailEmailTemplate";
+        public const string RegistrationEmailTemplatePath = "EmailTemplates.RegistrationEmailTemplate";
     }
 }
