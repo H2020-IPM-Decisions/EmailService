725d449a3ce43f323345c81b6462e080aa862373
Antonio Calatayud <antonio.calatayud@adas.co.uk>
2021-07-22 11:07:21 +0100

    Update Registration Email. Add hours

--- H2020.IPMDecisions.EML.Core/Dtos/RegistrationEmailDto.cs
+++ H2020.IPMDecisions.EML.Core/Dtos/RegistrationEmailDto.cs
@@ -1,6 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace H2020.IPMDecisions.EML.Core.Dtos
 {
     public class RegistrationEmailDto : EmailDto
     {
+        [Required]
+        public string CallbackUrl { get; set; }
+        public int HoursToConfirmEmail { get; set; } = 24;
     }
 }
