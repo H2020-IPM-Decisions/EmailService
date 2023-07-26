417f76a1ace2ceedf5c031416e5d9bd3e205d583
antonio-calatayud <antonio.calatayud@adas.co.uk>
2023-07-26 14:57:33 +0200

    New models and libs

---
+++ H2020.IPMDecisions.EML.Core/Models/ReportUserDataJoined.cs
@@ -0,0 +1,8 @@
+namespace H2020.IPMDecisions.EML.Core.Models
+{
+    public class ReportUserDataJoined
+    {
+        public ReportDataFarm FarmData { get; set; }
+        public ApplicationUserForReport User { get; set; }
+    }
+}
